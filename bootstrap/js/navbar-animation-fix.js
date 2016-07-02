d59f8b92924cdcd1ebc0b7755d6911900d066397
adrielsales <adrielsalles@gmail.com>
2016-07-02 12:45:07 -0300

    após concluir o curso :)

---
+++ bootstrap/js/navbar-animation-fix.js
@@ -0,0 +1,7 @@
+$('#meu-menu').on('show.bs.collapse', function(){
+	$('.topcasafina-banner').css('transform', 'translate(-50%, 10%');
+});
+
+$('#meu-menu').on('hide.bs.collapse', function(){
+	$('.topcasafina-banner').css('transform', 'translate(-50%, -50%');
+});
